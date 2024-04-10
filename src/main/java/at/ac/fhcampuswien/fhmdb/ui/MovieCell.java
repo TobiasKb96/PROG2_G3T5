e9791fee61b4a647d33d189bab17bc22c0e4a441
TobiasKb96 <tobias.kaltenbrunner1@stud.fh-campuswien.ac.at>
2024-04-11 00:01:29 +0200

    added rating and releaseYear to MovieCell

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -6,14 +6,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
+    private final Label releaseYear = new Label();
     private final Label detail = new Label();
     private final Label genres = new Label();
-    private final VBox layout = new VBox(title, detail, genres);
+    private final Label rating = new Label();
+
+    private final HBox firstline = new HBox(title, releaseYear);
+    private final HBox secondline = new HBox(detail);
+    private final HBox thirdline = new HBox(genres, rating);
+    private final VBox layout = new VBox(firstline, secondline, thirdline);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -25,23 +32,40 @@ public class MovieCell extends ListCell<Movie> {
         } else {
             this.getStyleClass().add("movie-cell");
             title.setText(movie.getTitle());
+            releaseYear.setText("(" + movie.getReleaseYear() + ")");
             detail.setText(
                     movie.getDescription() != null
                             ? movie.getDescription()
                             : "No description available"
             );
             genres.setText(movie.getGenresListAsString());
-
+            rating.setText("Rating: " + movie.getRating());
             // color scheme
             title.getStyleClass().add("text-yellow");
+            releaseYear.getStyleClass().add("text-white");
             detail.getStyleClass().add("text-white");
             genres.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
+            releaseYear.fontProperty().set(releaseYear.getFont().font(12));
             detail.setMaxWidth(this.getScene().getWidth() - 30);
             detail.setWrapText(true);
+
+            //first line
+            firstline.setPadding(new Insets(0));
+            firstline.spacingProperty().set(10);
+            firstline.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+            //second line
+
+            //third line
+            thirdline.setPadding(new Insets(0));
+            thirdline.spacingProperty().set(10);
+            thirdline.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+
+            //whole Movie
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
